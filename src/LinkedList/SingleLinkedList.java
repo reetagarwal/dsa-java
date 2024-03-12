059e6153d6d88da16a0bcef8d7dfcd70be63d1f5
Reet Agarwal <139364881+reetagarwal@users.noreply.github.com>
2024-03-12 09:26:19 +0530

    dsa

---
+++ src/LinkedList/SingleLinkedList.java
@@ -0,0 +1,291 @@
+package LinkedList;
+
+public class SingleLinkedList {
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data) {
+            this.data = data;
+            this.next=null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void add_beg(int data){//O(n)-constant time operation
+        Node newNode=new Node(data);//step1-creation of new node
+        size++;
+        if(head==null){
+           head=tail=newNode;
+           return;
+        }
+        newNode.next=head;//step2;linking
+        head=newNode;//step3
+    }
+
+    public void add_end(int data){//O(n)-constant time operation
+        Node newNode=new Node(data);//step1-creation of new node
+        size++;
+        if(head ==null){
+            head=tail=newNode;
+        }
+        tail.next=newNode;
+        tail=newNode;
+    }
+
+    public void add_pos(int index,int data){//O(n)-since we need to find the index so that takes time
+        if(index==0){
+            add_beg(data);
+            return;
+        }
+        Node newNode=new Node(data);
+        size++;
+        Node temp=head;
+        int i=0;//to track temp is at which index
+        while(i<index-1){
+            temp=temp.next;
+            i++;
+        }
+        //i=index-1;temp->prev
+        newNode.next=temp.next;
+        temp.next=newNode;
+    }
+
+    public void delete_beg(){//this just deletes the head node
+        if(head==null){
+            System.out.println("Nothing to delete");
+            return;
+        }
+        if(head.next==null){
+            head=tail=null;
+            size=0;
+        }
+        else{
+            head=head.next;
+            size--;
+        }
+    }
+
+    public int delete_first(){//this deletes the node and as well as return the value that you are deleting
+        if(head==null){
+            System.out.println("linked list is empty");
+            return Integer.MIN_VALUE;
+        }
+        if(head.next==null){
+            int val=head.data;
+            head=tail=null;
+            size=0;
+            return val;
+        }
+        int val=head.data;
+        head=head.next;
+        size--;
+        return val;
+    }
+
+//    public void delete_end(){//this deletes the last node simply
+//        if(head==null){
+//            System.out.println("Linked list is empty");
+//            return;
+//        }
+//        if(head.next==null){
+//            head=tail=null;
+//            size=0;
+//            return;
+//        }
+//        else{
+//            Node t=head;
+//            while(t.next.next==null){
+//                t=t.next;
+//            }
+//            t.next=null;
+//            t=t.next;
+//        }
+//    }
+
+    public int delete_last(){//this deletes the node and as well as returns the value that you are deleting
+        if(head==null){//if(size==0)
+            System.out.println("List is empty");
+            return Integer.MIN_VALUE;
+        }
+        if(head.next==null){//case where there's only a single node
+            int val=head.data;
+            head=tail=null;
+            //size=0;
+            return val;
+        }
+        //prev:i=size-2
+        Node prev=head;
+        for(int i=0;i<size-2;i++){
+            prev=prev.next;
+        }
+        int val=prev.next.data;//tail.data
+        prev.next=null;
+        tail=prev;
+        size--;
+        return val;
+    }
+
+    //find and remove nth node from end
+    public void deleteNthFromEnd(int n){
+        //calculate size
+        int size=0;
+        Node temp=head;
+        while(temp!=null){
+            temp=temp.next;
+            size++;
+        }
+        if(n==size){//this is the corner case for deleting head
+            head=head.next;//remove first
+            return;
+        }
+        int i=1;
+        Node prev=head;
+        while(i < (size - n)){
+            prev=prev.next;
+            i++;
+        }
+        prev.next=prev.next.next;
+        return;
+    }
+
+    public void search(int ele){
+        Node temp=head;
+        while(temp.data!=ele && temp.next!=null){
+            temp=temp.next;
+        }
+        if(temp.data==ele){
+            System.out.println("element is present");
+        }
+        if(temp.data!=ele){
+            System.out.println("element is not present");
+        }
+    }
+
+    public int itrSearch(int key){
+        Node temp=head;
+        int i=0;
+        while(temp!=null){
+            if(temp.data==key){//key found
+                return i;
+            }
+            temp=temp.next;
+            i++;
+        }
+        return -1;//key not found
+    }
+
+    public int helper(Node head,int key){
+        if(head==null){
+            return -1;
+        }
+        if(head.data==key){
+            return 0;
+        }
+        int index=helper(head.next,key);
+        if(index==-1){
+            return -1;
+        }
+        return index+1;
+    }
+    public int recSearch(int key){//searching using recursion
+        return helper(head,key);
+    }
+
+    public void reverse(){
+        Node t1=head;//t1 means prev
+        Node t2=t1.next;//t2 means curr
+        Node t3=t2.next;//t3 means next
+        t1.next=null;
+        t2.next=t1;
+        while(t3!=null){
+            t1=t2;
+            t2=t3;
+            t3=t3.next;
+            t2.next=t1;
+        }
+        head=t2;
+    }
+
+    public Node findMid(Node head){//slow-fast approach
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;//+1
+            fast=fast.next.next;//+2
+        }
+        return slow;//slow is my middle node
+    }
+
+    public boolean palindrome(){
+        if(head==null || head.next==null){
+            return true;
+        }
+        Node midNode=findMid(head);
+        Node prev=null;
+        Node current=midNode;
+        Node next;
+        while(current!=null){
+            next=current.next;
+            current.next=prev;
+            prev=current;
+            current=next;
+        }
+        Node right=prev;//right half head
+        Node left=head;
+        while(right!=null){
+            if(left.data!=right.data){
+                return false;
+            }
+            left=left.next;
+            right=right.next;
+        }
+        return true;
+    }
+
+    public void display(){
+        Node temp=head;
+        if(head==null){
+            System.out.println("Empty,there's nothing to display");
+            return;
+        }
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+       SingleLinkedList ll=new SingleLinkedList();
+       ll.add_beg(2);
+       ll.add_beg(1);
+//       ll.add_end(3);
+//       ll.add_end(4);
+//       ll.add_pos(2,7);
+        System.out.println(ll.palindrome());
+       ll.display();
+//       ll.delete_beg();
+////     ll.delete_end();
+//       ll.delete_last();
+//       ll.search(2);
+//       System.out.println(ll.itrSearch(10));
+//        System.out.println(ll.recSearch(10));
+//        ll.display();
+//        ll.reverse();
+//       ll.display();
+//       ll.deleteNthFromEnd(3);
+//       ll.display();
+//     System.out.println(ll.size);
+    }
+}
+
+
+/*Palindrome steps:
+first find mid-node(turtle-hare technique/slow-fast technique)
+then 2nd half reverse
+and then check if first half = second half
+ */
