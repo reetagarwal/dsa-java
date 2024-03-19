3dc35ef2dec651f071589eefcc67138229443515
Reet Agarwal <139364881+reetagarwal@users.noreply.github.com>
2024-03-19 16:10:35 +0530

    dsa

--- src/LinkedList/SingleLinkedList.java
+++ src/LinkedList/SingleLinkedList.java
@@ -353,6 +353,39 @@ public class SingleLinkedList {
         return mergedLl.next;
     }
 
+    public void zigzag(){
+        Node slow=head;
+        Node fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        Node mid=slow;
+
+        Node curr=mid.next;
+        mid.next=null;
+        Node prev=null;
+        Node next;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        Node left=head;
+        Node right=prev;
+        Node nextL,nextR;
+
+        while(left!=null && right!=null){
+            nextL=left.next;
+            left.next=right;
+            nextR=right.next;
+            right.next=nextL;
+            left=nextL;
+            right=nextR;
+        }
+    }
+
     public void display(){
         Node temp=head;
         if(head==null){
