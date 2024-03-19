827dd3c31f54071f338c4ccad0e4a1c5e627b32c
Reet Agarwal <139364881+reetagarwal@users.noreply.github.com>
2024-03-19 17:07:04 +0530

    dsa

---
+++ src/LinkedList/CircularLinkedList.java
@@ -0,0 +1,24 @@
+package LinkedList;
+
+public class CircularLinkedList {
+    public class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+        public static Node head;
+        public static Node tail;
+        public static int size;
+
+        public void add_beg(){
+
+        }
+
+        public static void main(String[] args) {
+            CircularLinkedList cll=new CircularLinkedList();
+        }
+
+}
