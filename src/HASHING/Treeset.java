c691514ebc8231f717d56f066b8914f49ef8363a
Reet Agarwal <139364881+reetagarwal@users.noreply.github.com>
2024-03-29 20:07:19 +0530

    Tree set

---
+++ src/HASHING/Treeset.java
@@ -0,0 +1,16 @@
+package HASHING;
+
+import java.util.TreeSet;
+
+public class Treeset {
+    public static void main(String[] args) {
+        TreeSet<String> set=new TreeSet<>();
+        set.add("Noida");
+        set.add("Chennai");
+        set.add("Bhubaneswar");
+        set.add("Mumbai");
+        System.out.println(set);
+    }
+}
+
+//sorted order
